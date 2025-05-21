2d06810a9887ad05ff967b632a2c77206b0d1563
Nikhil Yeli <yelin-iter@outlook.com>
2025-05-21 17:32:52 +0200

    route updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/npro", // Base URL for GitHub Pages
+  base: "/profile", // Base URL for GitHub Pages
   server: {
     host: "::",
     port: 8080,
